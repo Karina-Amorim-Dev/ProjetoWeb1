c47deee6d3873c5df24dc81734f6f5ba60151617
Karina <karina.a,orim.etec@gmail.com>
2026-05-14 19:54:36 -0300

    feat: Criando a Model Login

--- ProjetoWeb1/Models/LoginViewModel.cs
+++ ProjetoWeb1/Models/LoginViewModel.cs
@@ -2,5 +2,11 @@
 {
     public class LoginViewModel
     {
+        //n encapsulamento - modificadores get; e set;
+        public int Id { get; set; } 
+        public string Nome { get; set; }=string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Senha { get; set; } = string.Empty;
+        public string Nivel { get; set; } = "Funcionario";
     }
 }
